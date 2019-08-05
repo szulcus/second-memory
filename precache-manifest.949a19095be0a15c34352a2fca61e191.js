60e577f71547a91aba26639a9ff7253a40619902
jakub104 <jakubzlezena@wp.pl>
2019-08-05 22:11:33 +0200

    Updates

---
+++ precache-manifest.949a19095be0a15c34352a2fca61e191.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0963ec84760a0da3e440f655835368e3",
+    "url": "/second-memory/index.html"
+  },
+  {
+    "revision": "f7d7b4ad88fa453192cd",
+    "url": "/second-memory/static/css/main.1c50128a.chunk.css"
+  },
+  {
+    "revision": "cfa8747c965ea3388fd6",
+    "url": "/second-memory/static/js/2.c61ec8cf.chunk.js"
+  },
+  {
+    "revision": "f7d7b4ad88fa453192cd",
+    "url": "/second-memory/static/js/main.368895c5.chunk.js"
+  },
+  {
+    "revision": "1010653d8d14ec515e35",
+    "url": "/second-memory/static/js/runtime~main.2b7e4671.js"
+  },
+  {
+    "revision": "74b52e9d36fda2c942d0fd25309f9d9d",
+    "url": "/second-memory/static/media/instagram-d.74b52e9d.png"
+  },
+  {
+    "revision": "c5e01a9e81fb6a1bd6ce32e435bd1db8",
+    "url": "/second-memory/static/media/instagram-icon.c5e01a9e.png"
+  },
+  {
+    "revision": "8a2fabccfad38d9158808e24891ddf2a",
+    "url": "/second-memory/static/media/snapchat-d.8a2fabcc.png"
+  },
+  {
+    "revision": "ecfbbab3efd3b926c04a88875fea969b",
+    "url": "/second-memory/static/media/snapchat-icon.ecfbbab3.png"
+  },
+  {
+    "revision": "909d1f309e06a7fac892da5600c5bebd",
+    "url": "/second-memory/static/media/wow.909d1f30.png"
+  }
+]);
